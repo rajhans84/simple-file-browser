e33ff8db21811cc46e3f9eb89a01df5fa83d045f
Hans Raj Bhardwaj <rajhans.nz@gmail.com>
2015-09-25 00:23:34 +1200

    Implementation of filebrowser using interface

--- filebrowser.php
+++ filebrowser.php
@@ -2,5 +2,62 @@
 	require_once('interface.php');
 
 	class FileBrowser implements __FileBrowser {
+        var $rootPath;
+        var $currentPath;
+        var $extensionFilter;
 
-	}
+        function __construct($rootPath, $currentPath = null, array $extensionFilter = array()){
+            $this->rootPath = $rootPath;
+            if($currentPath){
+                $this->currentPath = $currentPath;
+            }else{
+                $this->currentPath =  $this->rootPath;
+            }
+            $this->extensionFilter = $extensionFilter;
+        }
+
+        function SetRootPath($rootPath){
+            $this->rootPath = $rootPath;
+        }
+        function SetCurrentPath($currentPath){
+                $this->currentPath = $currentPath;
+        }
+        function SetExtensionFilter(array $extensionFilter){
+            $this->extensionFilter = $extensionFilter;
+        }
+
+        /**
+         * Get files using currently-defined object properties
+         * @return array Array of files within the current directory
+         */
+        function Get()
+        {
+            $csv_extensions = implode(",", $this->extensionFilter);
+            $dirs = array();
+            $files = array();
+            $alldirs = glob("$this->currentPath/*", GLOB_ONLYDIR);
+            foreach($alldirs as $key=>$dir){
+                $dirs[$key]['name'] = basename($dir);
+                $dirs[$key]['path'] = $dir;
+            }
+            $allfiles = glob("$this->currentPath/*.{{$csv_extensions}}", GLOB_BRACE);
+            foreach($allfiles as $key=>$file){
+                $files[$key]['name'] = basename($file);
+                $files[$key]['path'] = $file;
+            }
+            $contents['files'] = $files;
+            $contents['dirs'] = $dirs;
+
+            return $contents;
+        }
+
+        function Up(){
+            if($this->currentPath == $this->rootPath){
+                return;
+            }else{
+                echo $this->currentPath = dirname($this->currentPath);
+            }
+        }
+
+
+    }
--- index.php
+++ index.php
@@ -4,13 +4,47 @@
 	/*
 	 * Add your filebrowser definition code here
 	 */
+
+    if($_GET['path']){
+        $currentDirPath = $_GET['path'];
+    }
+
+    $filebrowser = new FileBrowser('/home/raj/', $currentDirPath, array('md','php'));
+    if(isset($_GET['up']) && $_GET['up']==1 ){
+        $filebrowser->Up();
+    }
+    $contents = $filebrowser->get();
+
+
+
 ?>
 <!doctype html>
 <html lang="en">
  <head>
   <title>File browser</title>
+     <style>
+         .typeofdir{
+             background: url("dir.gif") top left no-repeat;
+             padding-left: 16px;
+
+         }
+         .typeoffile {
+             background: url("file.png") top left no-repeat;
+             padding-left: 16px;
+         }
+     </style>
  </head>
  <body>
   <!-- Output file list HTML here -->
+ <ul>
+    <li><a href="/?up=1">Up</a></li>
+     <?php  foreach($contents['dirs'] as $dir):      ?>
+     <li class="typeofdir"><a href="/?path=<?= $dir['path'] ?>"><?=$dir['name']?></a></li>
+     <?php endforeach;?>
+     <?php  foreach($contents['files']  as $file):      ?>
+     <li class="typeoffile"><?=$file['name']?></li>
+     <?php endforeach;?>
+
+ </ul>
  </body>
 </html>
